cf1ef64237efadc81c2623307ed3e5c614ebfa68
赵玉玲 <2872667714@qq.com>
2019-12-05 13:28:59 +0800

    更新2

--- src/wugong/Dugujiujian.java
+++ src/wugong/Dugujiujian.java
@@ -5,6 +5,7 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("11111111");
+		System.out.println("22222222");
 	}
 
 }
